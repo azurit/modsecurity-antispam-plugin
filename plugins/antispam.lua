e0c62bc36bef8ebe9fed6142694ac1a8a13b8c48
azurit <jozef@sudolsky.sk>
2024-03-30 14:10:32 +0100

    Update antispam.lua

--- plugins/antispam.lua
+++ plugins/antispam.lua
@@ -1,3 +1,12 @@
+-- -----------------------------------------------------------------------
+-- OWASP CRS Plugin
+-- Copyright (c) 2023-2024 Core Rule Set project. All rights reserved.
+--
+-- The OWASP CRS plugins are distributed under
+-- Apache Software License (ASL) version 2
+-- Please see the enclosed LICENSE file for full details.
+-- -----------------------------------------------------------------------
+
 function main(arg_name_to_scan)
 	pcall(require, "m")
 	local ok, http = pcall(require, "socket.http")
